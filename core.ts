da84e3f953000c551cfdd8d5de281918a4ad3350
chemthunder <chemthunderisme@gmail.com>
2026-04-22 12:14:13 -0400

    first test

--- core.ts
+++ core.ts
@@ -30,6 +30,51 @@ namespace yggdrasil {
     export function createPersistentKey<T>(content: T, location: string): PersistentKey<T> {
         return new PersistentKey<T>(content, location);
     }
-}
 
- ////% groups='["Keys", "Branches", "Spools", "Other"]'
+    /**
+     * Clears an array of all of its elements.
+     * @param array The array to clear.
+     */
+    //% block="clear array $array"
+    //% blockId=yggcleararray
+    //% array.defl=myArray
+    export function clearArray(array: any[]) {
+        for (let val of array) {
+            array.removeElement(val);
+        }
+    }
+
+    /**
+     * Returns if the specified array contains the specified value.
+     * @param array The array to check.
+     * @param value The value to check for.
+     */
+    //% block="array $array contains value $value"
+    //% blockId=yggarraycontainsvalue
+    //% array.defl=myArray
+    //% value.defl=myValue
+    export function arrayContainsValue<T>(array: T[], value: T): boolean {
+        for (let val of array) {
+            if (val == value) {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Runs the given code for each element of an array.
+     * @param array The array to check.
+     * @param s The code to run
+     */
+    //% block="for each of array $array run $s"
+    //% blockId=yggforeachofarray
+    //% array.defl=myArray
+    //% s.defl=myFunction
+    export function forEach<T>(array: T[], s: (element: T) => void) {
+        for (let val of array) {
+            s(val);
+        }
+    }
+}
